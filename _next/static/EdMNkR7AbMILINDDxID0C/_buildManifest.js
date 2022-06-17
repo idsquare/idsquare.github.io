bc075e40b2fad152eb96f272b2ef33a17f0618c2
Guillaume <guillaume.tavernier@ircad.fr>
2022-06-17 16:59:19 +0200

    Publish

---
+++ _next/static/EdMNkR7AbMILINDDxID0C/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1ce3fcab21c69cb9.css","static/chunks/pages/index-8a92b0cb29dcf165.js"],"/_error":["static/chunks/pages/_error-37938b1eca0044ba.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
